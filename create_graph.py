fe797681d1d02a4ccd1da66ff48547c5d5cdbd55
Maximilian Gillespie <max.gillespie@max.gillespie-C02FJ0DHML7H>
2022-03-14 07:58:16 -0600

    graph demo mvp working :)

--- create_graph.py
+++ create_graph.py
@@ -20,6 +20,25 @@ def find_node_with_id(id:str):
     result.pretty_print()
     return result
 
+def find_people_who_visited_country(country, params):
+    if (params != None):
+        query = """MATCH (p:person)-[v:visited %s]->(c:country) WHERE c.name = '%s'
+            RETURN p.name, p.age, v.purpose, c.name""" % (params, country)
+        result = redis_graph.query(query, params, timeout=10)
+    else:
+        query = """MATCH (p:person)-[v:visited]->(c:country) WHERE c.name = '%s'
+            RETURN p.name, p.age, v.purpose, c.name""" % country
+        result = redis_graph.query(query, None, timeout=10)
+    
+    result.pretty_print()
+    
+    # Iterate through resultset
+    for record in result.result_set:
+        person_name = record[0]
+        person_age = record[1]
+        visit_purpose = record[2]
+        country_name = record[3]
+
 ############# GRAPH MUTATIONS #############
 def add_sample_nodes():
     global nodes
@@ -74,43 +93,7 @@ if __name__ == "__main__":
     add_sample_nodes()
     add_sample_edges()
 
-    query = """MATCH (p:person)-[v:visited {purpose:"pleasure"}]->(c:country)
-            RETURN p.name, p.age, v.purpose, c.name"""
-
-    result = redis_graph.query(query)
-
-    # Print resultset
-    result.pretty_print()
-
-    # Use parameters
-    params = {'purpose':"pleasure"}
-    query = """MATCH (p:person)-[v:visited {purpose:$purpose}]->(c:country)
-            RETURN p.name, p.age, v.purpose, c.name"""
-
-    result = redis_graph.query(query, params)
-
-    # Print resultset
-    result.pretty_print()
-
-    # Use query timeout to raise an exception if the query takes over 10 milliseconds
-    result = redis_graph.query(query, params, timeout=10)
-
-    # Iterate through resultset
-    for record in result.result_set:
-        person_name = record[0]
-        person_age = record[1]
-        visit_purpose = record[2]
-        country_name = record[3]
-
-    query = """MATCH p = (:person)-[:visited {purpose:"pleasure"}]->(:country) RETURN p"""
-
-    result = redis_graph.query(query)
-
-    # Iterate through resultset
-    for record in result.result_set:
-        path = record[0]
-        print(path)
-
+    find_people_who_visited_country('USA', None)
 
     # IF WANTED – DELETE GRAPH
     # redis_graph.delete()
